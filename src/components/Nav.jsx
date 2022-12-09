48eb0e71d19f641fb5406a83fbf5f8e129004820
Your Name <you@example.com>
2022-12-09 12:15:27 +0530

    UI changes

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {NavLink} from 'react-router-dom';
+
 import logo from '../images/logo.svg'
 import styled from "styled-components";
 
--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Home from './Home'
 
-import {Route, Routes, BrowserRouter} from 'react-router-dom'
+import {Route, Routes} from 'react-router-dom'
 
 function Pages() {
   return (
